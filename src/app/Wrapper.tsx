6cb3fa61681842aa809be31c3d0156baf78454e8
naman30 <72098614+NamanKeshari@users.noreply.github.com>
2023-07-27 03:50:26 +0530

    removed not needed dependency and added status bar

--- src/app/Wrapper.tsx
+++ src/app/Wrapper.tsx
@@ -1,11 +1,13 @@
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import useAppInitializer from "../hooks/useAppInitializer";
 import MyStack from "../navigation/navigation";
+import { StatusBar } from "expo-status-bar";
 
 export default function Wrapper() {
   useAppInitializer();
   return (
     <SafeAreaProvider>
+      <StatusBar style="auto" />
       <MyStack />
     </SafeAreaProvider>
   );
